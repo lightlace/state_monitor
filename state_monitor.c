d52cb982432f197b1d031f5523c3a72d299c6be3
lightlace <fabian.baumanis@gmx.de>
2017-08-09 15:12:31 +0200

    - initial commit

---
+++ state_monitor.c
@@ -0,0 +1,36 @@
+/*
+ * state_monitor.c
+ *
+ * Script for monitoring the state of a block device.
+ * Copyright (c) 2017 Fabian Baumanis <fbaumanis@suse.de>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+int main(void){
+
+  char buffer[16];
+  int sel_ret, fd;
+  fd_set rfds;
+
+  fd = open("/sys/block/sdb/device/state", O_RDONLY);
+
+  FD_ZERO(&rfds);
+  FD_SET(fd, &rfds);
+
+while (1){
+  sel_ret = select(fd+1, NULL, NULL, &rfds, NULL);
+  printf("%d\n", sel_ret);
+  if (sel_ret == 1){
+    read(fd, buffer, 32);
+    printf("Buffer value: %s\n", buffer);
+    lseek(fd,0L,SEEK_SET);
+  }
+}
+return 0;
+}
