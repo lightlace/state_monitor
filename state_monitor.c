10489f7040cfb473079bdcb9fb6ade74b61db9a7
lightlace <fabian.baumanis@gmx.de>
2017-08-10 13:58:03 +0200

    - added signal handling for CTRL-C

--- state_monitor.c
+++ state_monitor.c
@@ -11,6 +11,9 @@
 #include <sys/select.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <signal.h>
+
+void signal_handler(int);
 
 int main(void){
 
@@ -22,17 +25,25 @@ int main(void){
 
   FD_ZERO(&rfds);
   FD_SET(fd, &rfds);
+  
+  signal(SIGINT, signal_handler);
 
-while (1){
-  sel_ret = select(fd+1, NULL, NULL, &rfds, NULL);
-  printf("%d\n", sel_ret);
-  if (sel_ret == 1){
-    read(fd, buffer, 32);
-    printf("Buffer value: %s\n", buffer);
-    lseek(fd,0L,SEEK_SET);
-  } else if (sel_ret == -1 ){
-    printf("select syscall failed with %s\n", sel_ret);
+  while (1){
+    sel_ret = select(fd+1, NULL, NULL, &rfds, NULL);
+    printf("%d\n", sel_ret);
+    if (sel_ret == 1){
+      read(fd, buffer, 32);
+      printf("Buffer value: %s\n", buffer);
+      lseek(fd,0L,SEEK_SET);
+    } else if (sel_ret == -1 ){
+      printf("select syscall failed with %s\n", sel_ret);
+    }
   }
-}
 return 0;
+}
+
+void signal_handler(int sig)
+{
+  printf("Interrupt was sent, exiting..\n");
+  exit(1);
 }
